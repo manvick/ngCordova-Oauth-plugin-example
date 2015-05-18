eebea72e59d6c610ed0060464f63c57d57d7c8f7
manvick <manvick95@gmail.com>
2015-05-19 01:34:00 +0530

    ngCordova Oauth Plugin Example

---
+++ www/js/app.js
@@ -0,0 +1,30 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var ionicApp = angular.module('starter', ['ionic','ngCordova'])
+
+ionicApp.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+ionicApp.controller('myCtrl', function($scope, $cordovaOauth){
+
+  $scope.facebookLogin = function() {
+    $cordovaOauth.facebook("1603759056570891", ["email"]).then(function(result){
+        $scope.data = result.access_token;
+    }, function(error){
+      alert("Error : " + error);
+    });
+  }
+});
